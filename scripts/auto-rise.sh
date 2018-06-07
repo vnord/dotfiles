efa1d9bbdc1885a973bacea86811dee51d747322
Ari von Nordenskjöld <ariv@student.chalmers.se>
2018-06-07 18:50:02 +0200

    fixes to screen layouts

--- scripts/auto-rise.sh
+++ scripts/auto-rise.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+THISHOST=$(hostname -s)
+
 xrandr --properties | grep "DP1 connected 2560"
 
-if [ $? -eq 0 ]
+if [ $? -eq 0 ] && [ "$THISHOST" = "gormenghast" ];
 then
     echo "setting main screen automatically"
     sh ~/.dotfiles/scripts/rise-normal.sh
--- scripts/home-double.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-xrandr --output DP1 --auto
-xrandr --output DP1 --primary
-xrandr --output DVI-D-0 --auto
-xrandr --output DVI-D-0 --right-of-DP1
-sh $HOME/.dotfiles/scripts/bg_set.sh
