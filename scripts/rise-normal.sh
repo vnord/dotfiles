158b17bbcb29a8257b5c988b77ad7e1df9a5ffe9
vnord <admin@vnord.net>
2018-07-02 14:18:07 +0200

    add laptop screen to rise work setup

--- scripts/rise-normal.sh
+++ scripts/rise-normal.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 xrandr --output DP1 --auto
-xrandr --output eDP1 --off
+# xrandr --output eDP1 --off
+xrandr --output eDP1 --auto
 xrandr --output DP1 --rotate normal
+xrandr --output eDP1 --below DP1
 sh $HOME/.dotfiles/scripts/bg_set.sh
